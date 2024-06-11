1d85d35a3a1c4ac9a6da610c55ac221b1d345110
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-06-11 17:40:48 -0300

    feat: Refactor withAuthAdmin component to handle authentication logic

--- components/withAuthAdmin/index.tsx
+++ components/withAuthAdmin/index.tsx
@@ -1,23 +1,26 @@
 import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import Cookie from 'js-cookie';
-
+import React, { ComponentType } from 'react';
 import AuthState from '../../dtos/AuthState';
 import User from '../../dtos/User';
 import ApiData from '../../dtos/ApiData';
 
-const withAuthAdmin = (Component) => {
-  const Auth = (props) => {
+const withAuthAdmin = (Component: ComponentType<any> & { getServerSideProps?: any }) => {
+  const Auth = (props: React.ComponentProps<typeof Component>) => {
   const router = useRouter();
   const loggedUser: User = useSelector((state: AuthState) => state.auth.loggedUser);
-  const apiData: ApiData = JSON.parse(Cookie.get('@api-data'));
+
+  const apiDataCookie = Cookie.get('@api-data');
+  const apiData: ApiData = apiDataCookie ? JSON.parse(apiDataCookie) : null;
 
   if(!loggedUser ||
     loggedUser.profile !== 'admin' ||
     !apiData ||
     !apiData['access-token'] ||
     apiData['access-token'] === '') {
-    router.push('/Auth/Login')
+    router.push('/Auth/Login');
+    return null;
   }
 
     return <Component {...props} />;
