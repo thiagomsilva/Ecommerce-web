a47c148ec4f982fc4753490be525db18b69e1978
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-06-11 17:41:04 -0300

    chore: Refactor Admin index page to use JSX syntax correctly

--- pages/Admin/index.tsx
+++ pages/Admin/index.tsx
@@ -3,9 +3,11 @@ import withAuthAdmin from "@/components/withAuthAdmin";
 import AdminComponent from "@/components/shared/AdminComponent";
 
 const Home: React.FC = () => {
-  <AdminComponent>
-    return <h1>Painel Admin</h1>
-  </AdminComponent>;
+  return (
+    <AdminComponent>
+      <h1>Painel Admin</h1>
+    </AdminComponent>
+  );
 };
 
 export default withAuthAdmin(Home);
