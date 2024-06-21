c0f67682e7314ddf25120ea03f26276582411dd3
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-06-20 23:46:29 -0300

    feat: Add search reducer and update SearchAndIcon component with search functionality and redirect to new path

--- components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
+++ components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
@@ -1,23 +1,67 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import styles from "../../../../styles/AdminTitle.module.css";
 import { InputGroup, FormControl, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import styles from "../../../../styles/AdminTitle.module.css";
 import StyledButton from "../../StyledButton";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import Link from "next/link";
 
+// realizamos a importação do dispatch e das actions para manipularmos a pesquisa no redux
+import { useDispatch } from "react-redux";
+import {
+  setSearch as setSearchRedux,
+  clearSearch,
+} from "../../../../store/modules/admin/shared/search/reducer";
+
+import { useRouter } from "next/router";
+
+// alteramos as propriedades do  componente para receber o newPath que será utilizado para redirecionar o usuário para o form de criação dos recursos
 interface SearchAndIcon {
   icon: IconProp;
+  newPath: string;
 }
 
-const SearchAndIcon: React.FC<SearchAndIcon> = ({ icon }) => {
+const SearchAndIcon: React.FC<SearchAndIcon> = ({ icon, newPath }) => {
+  // criamos um estado para podermos receber o que o usuário digitou no input de pesquisa
+  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  // limpando a pesquisa quando o component for renderizado
+  useEffect(() => {
+    dispatch(clearSearch());
+  }, []);
+
+  // método para realizar a tratativa da pesquisa. Ele irá atualizar o valor da pesquisa no redux
+  // esse método será chamado quando o usuário der enter no input de pesquisa ou clicar no ícone da pesquisa
+  const handleSearch = (): void => {
+    // toda vez que o termo de pesquisa for alterado a página será alterada para 1.
+    // utilizamos o método replace que tem a mesma função do metódo push,
+    // onde o mesmo não adiciona mais uma entrada no history do browser.
+    router.replace(router.pathname, "?page=1");
+    dispatch(setSearchRedux(search));
+  };
+
   return (
     <Row>
       <Col lg={9} xs>
         <Row>
           <Col lg={9} xs={10}>
             <InputGroup>
-              <FormControl placeholder="Pesquisar" className={styles.input} />
+              <FormControl
+                placeholder="Pesquisar usuário"
+                className={styles.input}
+                value={search}
+                onChange={(evt: React.ChangeEvent<HTMLInputElement>) => {
+                  setSearch(evt.target.value);
+                }}
+                onKeyPress={(evt: React.KeyboardEvent<HTMLInputElement>) => {
+                  if (evt.key.toLowerCase() === "enter") {
+                    handleSearch();
+                  }
+                }}
+              />
             </InputGroup>
           </Col>
 
@@ -27,15 +71,18 @@ const SearchAndIcon: React.FC<SearchAndIcon> = ({ icon }) => {
               size="lg"
               color="var(--color-gray-light)"
               className="float-left"
+              onClick={handleSearch}
             />
           </Col>
         </Row>
       </Col>
 
       <Col lg={2} xs={{ span: 3 }} className={styles.titleButton}>
-        <Col>
-          <StyledButton icon={icon} type_button="blue" />
-        </Col>
+        <Link href={newPath}>
+          <a>
+            <StyledButton icon={icon} type_button="blue" />
+          </a>
+        </Link>
       </Col>
     </Row>
   );
