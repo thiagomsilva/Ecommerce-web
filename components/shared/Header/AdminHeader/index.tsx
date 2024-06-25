4518c95d7fedc6844b025f50d7616400331a071b
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-06-25 01:45:15 -0300

    refactor: Update AdminHeader component to use larger column size for icons and add styles for icon spacing

--- components/shared/Header/AdminHeader/index.tsx
+++ components/shared/Header/AdminHeader/index.tsx
@@ -21,59 +21,59 @@ interface AdminHeaderProps {
 const AdminHeader: React.FC<AdminHeaderProps> = ({ name }) => {
   return (
     <Row className={styles.background}>
-      <Col lg={6} xs>
+      <Col lg={10} xs>
         <Link href="/Admin">
           <FontAwesomeIcon
               icon={faSignal}
               color="var(--color-gray-light)"
-              className="ml-3"
+              className={styles.icon}
             />
         </Link>
         <Link href="/Admin/Users/List">
           <FontAwesomeIcon
               icon={faUser}
               color="var(--color-gray-light)"
-              className="ml-3"
+              className={styles.icon}
             />
         </Link>
         <Link href="/Admin/Products/List">
           <FontAwesomeIcon
               icon={faGamepad}
               color="var(--color-gray-light)"
-              className="ml-3"
+              className={styles.icon}
             />
         </Link>
         <Link href="/Admin/Categories/List">
             <FontAwesomeIcon
               icon={faCheckSquare}
               color="var(--color-gray-light)"
-              className="ml-3"
+              className={styles.icon}
             />
         </Link>
         <Link href="/Admin/SystemRequirements/List">
           <FontAwesomeIcon
               icon={faLaptop}
               color="var(--color-gray-light)"
-              className="ml-3"
+              className={styles.icon}
             />
         </Link>
         <Link href="/Admin/Coupons/List">
           <FontAwesomeIcon
               icon={faTicketAlt}
               color="var(--color-gray-light)"
-              className="ml-3"
+              className={styles.icon}
             />
         </Link>
         <Link href="#">
           <FontAwesomeIcon
               icon={faDollarSign}
               color="var(--color-gray-light)"
-              className="ml-3"
+              className={styles.icon}
             />
         </Link>
       </Col>
 
-      <Col lg={6} xs>
+      <Col lg={2} xs>
         <div className="float-right">
           <span className={styles.name}>
             {name}
